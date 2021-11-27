072f5bc4925c00a65a919efe48f38ca3b291ce22
Eliezer Almeida <eliezeralmeidago@hotmail.com>
2021-11-27 16:43:34 -0300

    Implementado parte 3 do tutorial

---
+++ PublishSubscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+    var message = GetMessage();
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+
+    Console.WriteLine(" [x] Sent {0}", message);
+}
+
+Console.WriteLine(" [*] End");
+
+string GetMessage()
+{
+    return args.Length > 0
+        ? string.Join(" ", args)
+        : throw new ArgumentException("EmitLog need message in args");
+}
---
+++ PublishSubscribe/ReceiveLog/ReceiveLog.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+    var queueName = channel.QueueDeclare().QueueName;
+    channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(" [*] Waiting for logs.");
+    Console.ResetColor();
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (sender, ea) =>
+    {
+        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+        Console.ForegroundColor = ConsoleColor.Blue;
+        
+        Console.WriteLine(" [x] {0}", message);
+        Console.ResetColor();
+    };
+
+    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
