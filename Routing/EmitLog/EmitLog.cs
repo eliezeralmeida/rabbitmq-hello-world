739ce7a824a586e96744616231a08401f27a4e3f
Eliezer Almeida <eliezeralmeidago@hotmail.com>
2021-11-27 20:12:58 -0300

    Adicioando exemplo de routing

---
+++ Routing/EmitLog/EmitLog.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+    var serverity = GetSeverity();
+    var message = GetMessage();
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct_logs",
+        routingKey: serverity,
+        basicProperties: null,
+        body: body);
+
+    Console.ForegroundColor = GetConsoleColor(serverity);
+    Console.WriteLine(" [x] Sent {0}: {1}", serverity, message);
+    Console.ResetColor();
+}
+
+Console.WriteLine(" [*] End");
+
+string GetSeverity()
+{
+    return args.Length > 0
+        ? args[0].ToUpper()
+        : throw new ArgumentException("EmitLog need severity at first args: Info, Warning or Error");
+}
+
+string GetMessage()
+{
+    return args.Length > 1
+        ? string.Join(" ", args.Skip(1).ToArray())
+        : throw new ArgumentException("EmitLog need message at second args");
+}
+
+ConsoleColor GetConsoleColor(string severity)
+{
+    return severity switch
+    {
+        "ERROR" => ConsoleColor.Red,
+        "WARNING" => ConsoleColor.Yellow,
+        _ => ConsoleColor.Blue
+    };
+}
---
+++ Routing/ReceiveLog/ReceiveLog.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+var queueName = channel.QueueDeclare().QueueName;
+var severities = GetSeverities();
+
+foreach (var severity in severities)
+{
+    channel.QueueBind(
+        queue: queueName,
+        exchange: "direct_logs",
+        routingKey: severity);
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(" [*] Waiting for: {0}", string.Join(", ", severities));
+Console.ResetColor();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    var severity = ea.RoutingKey;
+
+    Console.ForegroundColor = GetConsoleColor(severity);
+    Console.WriteLine(" [x] {0}: {1}", severity, message);
+    Console.ResetColor();
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+string[] GetSeverities()
+{
+    return args.Length > 1
+        ? args.Select(i => i.ToUpper()).ToArray()
+        : throw new ArgumentException("Usage Args: [info] [warning] [error]");
+}
+
+ConsoleColor GetConsoleColor(string severity)
+{
+    return severity switch
+    {
+        "ERROR" => ConsoleColor.Red,
+        "WARNING" => ConsoleColor.Yellow,
+        _ => ConsoleColor.Blue
+    };
+}
