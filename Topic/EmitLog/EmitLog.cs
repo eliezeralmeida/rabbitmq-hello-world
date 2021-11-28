9536469a905c7f25a9d3164dc41bf55feb9c1da9
Eliezer Almeida <eliezeralmeidago@hotmail.com>
2021-11-27 21:00:58 -0300

    Adicionado exemplo com exange topic

---
+++ Topic/EmitLog/EmitLog.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+    var topicKey = GetTopicKey();
+    var message = GetMessage();
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "topic_logs",
+        routingKey: topicKey,
+        basicProperties: null,
+        body: body);
+
+    Console.ForegroundColor = GetConsoleColor(topicKey);
+    Console.WriteLine(" [x] Sent {0}: {1}", topicKey, message);
+    Console.ResetColor();
+}
+
+Console.WriteLine(" [*] End");
+
+string GetTopicKey()
+{
+    return args.Length > 0
+        ? args[0].ToLower()
+        : throw new ArgumentException(
+            "Need the topic at first args, ex: info.auth warn.network error.payment");
+}
+
+string GetMessage()
+{
+    return args.Length > 1
+        ? string.Join(" ", args.Skip(1).ToArray())
+        : throw new ArgumentException("Need the message at second args");
+}
+
+ConsoleColor GetConsoleColor(string topic)
+{
+    return topic.Split(".")[0] switch
+    {
+        "error" => ConsoleColor.Red,
+        "warn" => ConsoleColor.Yellow,
+        _ => ConsoleColor.Blue
+    };
+}
---
+++ Topic/ReceiveLog/ReceiveLog.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+var queueName = channel.QueueDeclare().QueueName;
+var bindingTopicKeys = GetTopicKeys();
+
+foreach (var bindingTopKey in bindingTopicKeys)
+{
+    channel.QueueBind(
+        queue: queueName,
+        exchange: "topic_logs",
+        routingKey: bindingTopKey);
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(" [*] Waiting for topic keys: {0}", string.Join(", ", bindingTopicKeys));
+Console.ResetColor();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    var topicKey = ea.RoutingKey;
+
+    Console.ForegroundColor = GetConsoleColor(topicKey);
+    Console.WriteLine(" [x] {0}: {1}", topicKey, message);
+    Console.ResetColor();
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+string[] GetTopicKeys()
+{
+    return args.Length > 1
+        ? args.Select(i => i.ToLower()).ToArray()
+        : throw new ArgumentException("Usage args ex: info.auth warn.* error.*");
+}
+
+ConsoleColor GetConsoleColor(string topicKey)
+{
+    return topicKey.Split(".")[0] switch
+    {
+        "error" => ConsoleColor.Red,
+        "warn" => ConsoleColor.Yellow,
+        "info" => ConsoleColor.Blue,
+        _ => ConsoleColor.DarkBlue
+    };
+}
